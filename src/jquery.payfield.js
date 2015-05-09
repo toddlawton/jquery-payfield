ceb59a0bf3e472240644470fa331907d3fa5e994
Todd Beauchamp <toddbee@gmail.com>
2015-05-09 19:50:09 -0400

    Added input regex checks

--- src/jquery.payfield.js
+++ src/jquery.payfield.js
@@ -40,14 +40,36 @@
 
 				bindEvents: function(el) {
 					var self = this;
-
-					el.bind('keydown', function(e){
+					
+					el.on('click', function(e){
+						if (self.settings.cardNumberCompleted) {
+							self.settings.cardNumberCompleted = !self.settings.cardNumberCompleted;
+							self.hideAdditionalFields();
+						}
+					})
+					.on('keydown', function(e){
+						var keypressed = e.which;
 						self.checkForSpaces(e);
-					});
-
-					el.bind('keyup', function(){
+					})
+					.on('keyup blur', function(){
 						var creditCardVendor = self.getcreditCardVendor(el.val());
 						self.switchCreditCardIcon(creditCardVendor);
+						if (self.$element.val().length === 19) {
+							self.showAdditionalFields(true);
+							self.settings.cardNumberCompleted = true;
+						}
+					});
+
+					el.closest('.'+this._name+'-container').find('input').each(function(){
+						var pattern = new RegExp($(this).attr('pattern'));
+
+						$(this).on('keydown', function(e) {
+							// If not delete and backspace and not an integer, prevent keypress
+							if (e.which !== 8 && e.which !== 46 && !pattern.test(String.fromCharCode(e.which))) {
+								e.preventDefault();
+								return false;
+							}
+						});
 					});
 				},
 
@@ -69,27 +91,40 @@
 							name: 'credit-card-expiration',
 							placeholder: 'MM/YY',
 							maxlength: 5,
+							pattern: '^[0-9]*$',
 							type: 'tel'
 						},
 						{
 							name: 'credit-card-cvv',
 							placeholder: 'CVV',
 							maxlength: 3,
+							pattern: '^[0-9]*$',
 							type: 'tel'
 						}],
 						inputMarkup = '';
 
 					$.each(inputList, function(index, attrs) {
-						inputMarkup += '<input class="'+attrs.name+'" placeholder="'+attrs.placeholder+'" maxlength="'+attrs.maxlength+'" type="'+attrs.type+'" />';
+						inputMarkup += '<input class="'+attrs.name+'" placeholder="'+attrs.placeholder+'" maxlength="'+attrs.maxlength+'" pattern="'+attrs.pattern+'" type="'+attrs.type+'" />';
 					});
 
 					inputMarkup = '<div class="additional-credit-card-fields">' + inputMarkup;
 					inputMarkup = inputMarkup + '</div>';
 
 					el.addClass(this._name+'-input') // Add a class to the input with the plugin name
+					  .attr('pattern', '^[0-9]*$') // Allow only integers in the card input
 					  .wrap(elContainer) // Wrap the input with a container div
 					  .parents('.'+this._name+'-container').prepend(elIconContainer) // Add a flag icon before the input inside of the container
 					  									   .append(inputMarkup); // Add additional credit card fields
+
+				    this.$icon = el.siblings('.'+this._name+'-icon');
+				    this.$additionalFields = el.siblings('.additional-credit-card-fields');
+				    this.$expirationField = this.$additionalFields.find('.credit-card-expiration');
+				    this.hideAdditionalFields();
+					this.settings.inputPadding = parseInt(el.css('padding-top'));
+					this.settings.iconBorderWidth = (el.outerWidth() - el.innerWidth()) / 2;
+					this.settings.iconWidth = el.height() * 1.6590909091;
+					this.settings.iconHeight = el.height();
+					this.settings.letterWidth = (parseInt(el.css('font-size')) / 1.5);
 				},
 
 				/**
@@ -147,19 +182,27 @@
 				 * depending on its current attributes
 				 */
 				setIconDimensions: function(el) {
-					this.$icon = el.siblings('.'+this._name+'-icon');
-					var inputPadding = parseInt(el.css('padding-top')),
-						iconBorderWidth = (el.outerWidth() - el.innerWidth()) / 2,
-						iconWidth = el.height() * 1.6590909091,
-						iconHeight = el.height();
-					
-					el.css('padding-left', iconWidth + inputPadding * 2 + 'px');
+					el.css('padding-left', this.settings.iconWidth + this.settings.inputPadding * 2 + 'px');
 					
 					this.$icon.css({
-						'width': iconWidth + 'px',
-						'height': iconHeight + 'px',
-						'top': inputPadding + iconBorderWidth + 'px'
+						'width': this.settings.iconWidth + 'px',
+						'height': this.settings.iconHeight + 'px',
+						'top': this.settings.inputPadding + this.settings.iconBorderWidth + 'px'
 					});
+				},
+
+				showAdditionalFields: function(triggerFocus) {
+					this.$element.css('text-indent', - this.settings.letterWidth * 12 - this.settings.letterWidth / 2 - 1 + 'px');
+					this.$additionalFields.css('left', this.settings.iconWidth + this.settings.inputPadding * 2 + (this.settings.letterWidth + 1) * 5 + 'px');
+					if (triggerFocus) {
+						this.$expirationField.trigger('focus');
+					}
+				},
+
+				hideAdditionalFields: function() {
+					this.$element.css('text-indent', '0px');
+					this.$additionalFields.css('left', this.$element.outerWidth() + 'px');
+
 				}
 		});
 
